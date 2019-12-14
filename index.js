298273d8f9ba0b8fd0509b22d18af958a6d0e285
sukem <tmfreq0825@gmail.com>
2019-12-14 15:41:25 +0900

    play

--- index.js
+++ index.js
@@ -1,7 +1,65 @@
-var http = require('http');
-http.createServer(function (request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello World\n');
-}).listen(3000);
+'use strict';
 
-console.log('Server running at http://127.0.0.1:3000');
+const socketIO = require('socket.io');
+
+const PORT = process.env.PORT || 3000;
+const io = socketIO.listen(PORT, {
+    serveClient: false,
+    // origins: ['*:*']
+});
+
+const rooms = [];
+
+io.on('connection', (socket) => {
+    console.log('Client connected');
+
+    socket.on('disconnect', () => {
+        const room = rooms.find(r => r.ids.includes(socket.id));
+        if (room) {
+            room.ids.splice(room.ids.findIndex(id => id === socket.id), 1);
+            if (room.ids.length === 0) {
+                rooms.splice(rooms.findIndex(r => r === room), 1);
+            } else {
+                socket.to(room.name).emit('room_info', {
+                    'name': room.name,
+                    'nop': room.ids.length,
+                    'state': 'pending',
+                    'seek': 0
+                });
+            }
+        }
+        // console.log(rooms.length);
+    });
+
+    socket.on('room_name', (name) => {
+        if (!name) return;
+        let room = rooms.find(r => r.name === name);
+        if (room) {
+            room.ids.push(socket.id);
+        } else {
+            room = {
+                'name': name,
+                'ids': [socket.id]
+            };
+            rooms.push(room);
+        }
+        socket.join(name);
+        const info = {
+            'name': name,
+            'nop': room.ids.length,
+            'state': 'pending',
+            'seek': 0
+        };
+        socket.to(name).emit('room_info', info);
+        socket.emit('room_info', info);
+        console.log('joined to ' + name);
+        // console.log(rooms[0].ids.length);
+    });
+
+    socket.on('play_request', () => {
+        const room = rooms.find(r => r.ids.includes(socket.id));
+        console.log('got request: ' + room.name);
+        socket.to(room.name).emit('play');
+        socket.emit('play');
+    })
+});
